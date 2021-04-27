389e44e48025f19266f2855761dfef617b804c69
Erick Contreras <Erickcc06@gmail.com>
2021-04-26 18:24:11 -0600

    Added fetch function for crypto API

---
+++ assets/js/fetchCryptoInfo.js
@@ -0,0 +1,43 @@
+/*
+Values to retrieve:
+-Name
+-Current price (USD)
+-All time high (USD)
+-Market Cap (USD)
+*/
+
+function fetchCryptoInfo(cryptoName){
+    var cryptoUrl = 'https://api.coingecko.com/api/v3/coins/' +
+    cryptoName +
+    '?localization=false' +
+    '&tickers=true&' +
+    'market_data=true&' +
+    'community_data=true&' +
+    'developer_data=true&' +
+    'sparkline=true';
+    var cryptoInfo;
+    
+    fetch(cryptoUrl)
+    .then(function (response) {
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
+    .then(function (locRes) {
+        // console.log("Name: " + locRes.name);
+        // console.log("Current Price: " + locRes.market_data.current_price.usd);
+        // console.log("All time high: " + locRes.market_data.ath.usd);
+        // console.log("Market Cap: " + locRes.market_data.market_cap.usd);
+        cryptoInfo = {
+            name: locRes.name,
+            currentPrice: locRes.market_data.current_price.usd,
+            allTimeHigh: locRes.market_data.ath.usd,
+            marketCap: locRes.market_data.market_cap.usd,
+        };
+        console.log(cryptoInfo);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
+};
---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+//Exchange rates:
+//https://api.coingecko.com/api/v3/exchange_rates
+
+//Compare prices:
+//https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,litecoin&vs_currencies=eth,usd,mxn
+
+//Supported currency exchages
+//https://api.coingecko.com/api/v3/simple/supported_vs_currencies
+
+//Main info link
+//https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true
+
+//NewsAPI
+//Key: c291ba41b0364ba5a0476ff14be9cef8
+
+var newsUrl = 'https://newsapi.org/v2/everything?' +
+    'q=+dogecoin&' +
+    'from=2021-04-01&' +
+    'language=en&' +
+    'sortBy=relevancy&' +
+    'apiKey=c291ba41b0364ba5a0476ff14be9cef8';
+
+function getCryptoInfo() {
+    var cryptoName = "bitcoin";
+    fetchCryptoInfo(cryptoName);
+}
+
+getCryptoInfo();
+
+
